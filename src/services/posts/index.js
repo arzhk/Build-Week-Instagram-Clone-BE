896d0b063857a797b21d38b7856f04f80aafd062
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-03-01 14:02:20 +0200

    Edit method added to posts

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -27,8 +27,21 @@ postsRouter.get("/", async (req, res, next) => {
 postsRouter.delete("/:id", async (req, res, next) => {
   try {
     const postToDelete = await postModel.findByIdAndDelete(req.params.id);
-
     res.status(204).send(postToDelete);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+postsRouter.put("/:id", async (req, res, next) => {
+  try {
+    const posttoEdit = await postModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { runValidators: true, new: true }
+    );
+    res.status(204).send(posttoEdit);
   } catch (error) {
     console.log(error);
     next(error);
