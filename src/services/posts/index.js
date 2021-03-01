7f140289e8224273781a9706ac032586a0bf83c8
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-03-01 13:41:23 +0200

    get method for posts added

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
-const PostModel = require("../posts/schema");
+const postModel = require("../posts/schema");
 
 const postsRouter = express.Router();
 
 postsRouter.post("/", async (req, res, next) => {
   try {
-    const newPost = new PostModel(req.body);
+    const newPost = new postModel(req.body);
     await newPost.save();
     res.status(201).send("Post has been added.");
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+postsRouter.get("/", async (req, res, next) => {
+  try {
+    const posts = await postModel.find();
+    res.status(200).send(posts);
   } catch (error) {
     console.log(error);
     next(error);
